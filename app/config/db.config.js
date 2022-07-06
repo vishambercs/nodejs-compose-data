7bf21f4bbee4fe77265a7c3ff2902a0a4093fb4c
Vishamber Lal <v.c@vaimanagement.co>
2022-07-06 17:31:00 +0400

    Changes

---
+++ app/config/db.config.js
@@ -0,0 +1,10 @@
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+module.exports = {
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+};
